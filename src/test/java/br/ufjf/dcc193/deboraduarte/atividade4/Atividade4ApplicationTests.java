03ae4d32a6fe1055b77f8473db57f3ac844be498
dduartevla <deboraduarte@ice.ufjf.br>
2023-04-04 21:15:57 -0300

    Commit inicial

---
+++ src/test/java/br/ufjf/dcc193/deboraduarte/atividade4/Atividade4ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.deboraduarte.atividade4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
